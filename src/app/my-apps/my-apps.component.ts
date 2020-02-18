8bae12d56abf4f5f3145ee7ec1ef94b6ea70e756
rduvvuri <rduvvuri@deloitte.com>
2020-02-18 14:45:10 -0500

    Merged My app UI code

--- src/app/my-apps/my-apps.component.ts
+++ src/app/my-apps/my-apps.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyAppsComponent implements OnInit {
 
-  constructor() { }
+  showDiv:boolean = false;
+  appDetails: any;
+
+  constructor() { 
+    this.appDetails = {
+      platform: "Docker",
+      scrumTeam: "Olympics",
+      jiraKey: "SE-314743",
+      requestType: "Microservices",
+      projectKey: "SOADAS",
+      serviceHealthURL: "www.sit-interlock.anthem.com",
+      serviceName: "App1",
+      contextRoot: "/tst/App1/1.0",
+      archetype: "docker-prototype-ms",
+      userID: "AD18279",
+      bitbucketURL: "www.bitbucket.anthem.com",
+      bambooURL: "www.bamboo.anthem.com"
+    }
+  }
 
   ngOnInit() {
+    
+  }
+
+  showDetails(){
+    this.showDiv = !this.showDiv;
   }
 
 }
