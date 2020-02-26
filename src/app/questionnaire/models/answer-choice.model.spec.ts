aa2b44dd2a2d5c6e2bbc67693952eae4eb74ad76
pruballa <pruballa@deloitte.com>
2020-02-25 18:25:35 -0600

    added initial deployment code

---
+++ src/app/questionnaire/models/answer-choice.model.spec.ts
@@ -0,0 +1,7 @@
+import { AnswerChoice } from './answer-choice.model';
+
+describe('AnswerChoice', () => {
+  it('should create an instance', () => {
+    expect(new AnswerChoice()).toBeTruthy();
+  });
+});
